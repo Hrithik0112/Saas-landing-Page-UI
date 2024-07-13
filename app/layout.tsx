be0eb82217b81c5e2334cd3c758c573482281307
Hrithik <slumdog4321@gmail.com>
2024-07-13 19:25:57 +0530

    Feat : Banner Component added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
+import clsx from "clsx";
 
-const inter = Inter({ subsets: ["latin"] });
+const dmSans= DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={clsx(dmSans.className , 'antialiased')}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { Banner } from "./_components/Banner";
 
 
 export default function Home() {
   return (
-    <div className="flex max-w-screen-2xl h-screen justify-center items-center bg-gradient-to-br from-white via-purple-300 to-purple-500">
-      <h1 className="text-3xl font-bold text-black text-center">Wake Up to reality</h1>
-    </div>
+    <>
+    <Banner/>
+    </>
   );
 }
