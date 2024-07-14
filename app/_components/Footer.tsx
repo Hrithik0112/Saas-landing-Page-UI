5ab50cbe253c414ce52efa5f27322f6cc30d50a0
Hrithik <slumdog4321@gmail.com>
2024-07-14 23:24:52 +0530

    Fix : Fixed Footer style and icon issue

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,23 +1,31 @@
-import Linkedin from "../../public/LinkedIN.svg"
-import Instagram from "../../public/Instagram.svg"
-import Slack from "../../public/Slack.svg"
-import Twitter from "../../public/Twitter.svg"
-import Image from "next/image"
+import Linkedin from "../../public/LinkedIN.svg";
+import Youtube from "../../public/youtube.svg";
+import TikTok from "../../public/tiktok.svg";
+import Twitter from "../../public/x-social.svg";
+import Image from "next/image";
 
-export const Footer = ()=> {
-    return (
-        <div className="py-5 bg-black">
-            <div className=" flex flex-col items-center sm:flex-row md:mx-auto gap-4 border-t border-white/30">
-
-            <h2 className="text-center text-white/60">© 2024 Hrithik, Inc. All rights reserved</h2>
-            <div className="flex gap-2.5 mx-auto items-center">
-                <Image src={Linkedin}  alt="" className="text-white/60 w-6 h-6 bg-white/60"/>
-                <Image src={Instagram} alt="" className="text-white/60 w-6 h-6 bg-white/60"/>
-                <Image src={Slack} alt="" className="text-white/60 w-6 h-6 bg-white/60"/>
-                <Image src={Twitter} alt="" className="text-white/60 w-6 h-6 bg-white/60"/>
-                
-            </div>
-            </div>
-        </div>
-    )
-}
+export const Footer = () => {
+  return (
+    <footer className="py-5 bg-black text-white/60 border-t border-white/30">
+    <div className="p-4 mx-auto" >
+      <div className=" flex flex-col sm:flex-row sm:justify-between gap-4 ">
+        <div className="text-center">© 2024 Hrithik, Inc. All rights reserved</div>
+        <ul className="flex gap-2.5 justify-center">
+          <li className="bg-white/60">
+            <Image src={Linkedin} alt=""  />
+          </li>
+          <li className="bg-white/60">
+            <Image src={Youtube} alt="" />
+          </li>
+          <li className="bg-white/60">
+            <Image src={TikTok} alt="" />
+          </li>
+          <li className="bg-white/60">
+            <Image src={Twitter} alt=""  />
+          </li>
+        </ul>
+      </div>
+    </div>
+    </footer>
+  );
+};
